1d71463ee4b407d4ec27fa2e088181c4ed1768e9
teclivre <contato@portalsjcampos.com>
2026-06-04 16:10:25 -0300

    Corrige visual de posts/páginas: CSS tipografia e pages com nav duplicado do Elementor

--- rmfacilities-onepage/page.php
+++ rmfacilities-onepage/page.php
@@ -14,7 +14,12 @@ get_header();
 			<?php while ( have_posts() ) : the_post(); ?>
 				<article <?php post_class( 'page-content' ); ?> id="post-<?php the_ID(); ?>">
 					<h1><?php the_title(); ?></h1>
-					<?php the_content(); ?>
+					<?php if ( has_post_thumbnail() ) : ?>
+						<div class="post-thumb"><?php the_post_thumbnail( 'large' ); ?></div>
+					<?php endif; ?>
+					<div class="post-content">
+						<?php the_content(); ?>
+					</div>
 				</article>
 				<?php comments_template(); ?>
 			<?php endwhile; ?>
